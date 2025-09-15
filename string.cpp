1b7e2ea287ac21623b3150ac1c07279c4d055ec6
OneMoveBRB <sh_amir2014@mail.ru>
2025-09-15 21:16:52 +0300

    Update string.cpp
    added getline_

--- string.cpp
+++ string.cpp
@@ -1,6 +1,8 @@
 #include "string.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 
 int strcmp_(const char* str1, const char* str2) {
@@ -22,4 +24,42 @@ int fputs_(const char* str, FILE* stream) {
     }
 
     return *(str-1);
+}
+
+ssize_t getline_(char** line, size_t* n, FILE* fp) {
+    assert( n != NULL );
+    assert( fp != NULL );
+
+
+    const int fst_size = 5;
+    const int exp_multiplier = 2;
+    if (*n == 0) {
+        *n = fst_size;
+        *line = (char *)calloc(*n, sizeof(char));
+    }
+
+    // FIXME n и line в переменную  // ЧТО это значит? Как?
+    int c = 0;
+    size_t line_size = 0;
+    while ( (c = getc(fp)) != EOF ) {
+        if (line_size + 2 > *n) {
+            *n *= exp_multiplier;
+            *line = (char*)realloc(*line, *n);
+        }
+        (*line)[line_size++] = (char)c;
+        if (c == '\n') break;
+    }
+
+    if (line_size == 0) {
+        *n = 0;
+        free(*line);
+        *line = NULL;
+        return EOF;
+    } else {
+        *n = line_size + 1;
+        *line = (char*)realloc(*line, *n);
+        (*line)[line_size] = '\0';
+    }
+
+    return (ssize_t)line_size;
 }
